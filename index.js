4b787c3089ae6ab800bd63dfadd3d7c4966d0d6a
cisshere <cis.shere@gmail.com>
2023-05-26 19:10:02 -0300

    agregando a js el menu hamburguesa

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const menuDespegable = document.querySelector(".menu-des");
-const navListPrincipal = document.querySelector(".nav-list-principal");
-
-
+const navListPrincipal = document.getElementById("nav");
 
 const despeglarMenu = () => {
-    navListPrincipal.classList.toggle("nav-desplegable");
     navListPrincipal.classList.toggle("nav-list-principal");
-
+    navListPrincipal.classList.toggle("nav-desplegable");
 }
 
+const onResize = () => {
+    if(window.innerWidth >= 1050){
+        navListPrincipal.classList.remove("nav-desplegable");
+        navListPrincipal.classList.add("nav-list-principal");
+    }
+}
 
 const init = () => {
     menuDespegable.addEventListener("click",despeglarMenu);
+    window.addEventListener("resize", onResize);
 }
 
 init();
