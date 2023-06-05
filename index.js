d462b6b879b0fbf52db2659a9fe1b92909265b2c
cisshere <cis.shere@gmail.com>
2023-06-05 18:16:54 -0300

    renderizando carrito

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const navegadorMenu = document.querySelector(".menu");
 
-const img = document.createElement('img');
+const elementos = ['Inicio', 'Productos', 'Beneficio', 'Contacto'];
+    
+
+ const menu = () => {
+
+    const img = document.createElement('img');
     img.src = 'img/menu.png';
     img.alt = "Menu despegable";
     img.classList = 'menu-des';
@@ -10,12 +15,6 @@ const img = document.createElement('img');
     const ul = document.createElement('ul');
     ul.id = 'nav';
     ul.classList = 'nav-list-principal';
-
-    const elementos = ['Inicio', 'Productos', 'Beneficio', 'Contacto'];
-    
-    
- const menu = () => {
-
 
     for (let i= 0; i < elementos.length; i++){
         const li = document.createElement("li");
@@ -35,11 +34,14 @@ menu();
 
 const menuDespegable = document.querySelector(".menu-des");
 const navListPrincipal = document.querySelector("#nav");
+const bolsaCompra = document.querySelector(".bolsa-compra");
+const carritoVacio = document.querySelector(".carrito");
+const navItem = document.querySelector(".nav-item-link");
 
 const despeglarMenu = () => {
     navListPrincipal.classList.toggle("nav-list-principal");
     navListPrincipal.classList.toggle("nav-desplegable");
-    navListPrincipal.classList.add("prueba");
+    carritoVacio.classList.remove("carrito-vacio");
 }
 
 const onResize = () => {
@@ -52,11 +54,16 @@ const onResize = () => {
  const Inicio = document.querySelector("#link-Inicio");
  const Productos = document.querySelector("#link-Productos");
 
-const itemLink = () => {
+const removeMenuDes = () => {
     
     navListPrincipal.classList.remove("nav-desplegable");
     navListPrincipal.classList.add("nav-list-principal");
 } 
+
+const carritoCompra = () =>{
+    carritoVacio.classList.toggle("carrito-vacio");
+    removeMenuDes();
+}
 
 const productosContenedor = document.querySelector(".productos-contenedor");
 
@@ -71,30 +78,54 @@ const mostrarProductos = (listaDeProductos) => {
             <p class="nombre-producto"> ${nombre} </p>
             <p class="precio"> $ ${precio} </p>
             <p class="tono"> ${tono} </p>
-            <a href="">Comprar</a>
+            <a class="btn-comprar" href="">Comprar</a>
         </div>
 
         `
     }).join("");
 } 
 
-const bolsaCompra = document.querySelector(".bolsa-compra");
-const carritoVacio = document.querySelector(".carrito");
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-const carritoCompra = () =>{
-    carritoVacio.classList.toggle("carrito-vacio");
+const saveCart = () => {
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
+
+const contenedorProductosCarrito = document.querySelector(".productos-contenedor-carrito");
+
+const renderCart = () => {
+
+    contenedorProductosCarrito.innerHTML = cart.map((productoCarrito => {
+        const { img, alt, nombre, precio, tono, cantidad} = productoCarrito;
+        return `
+        
+        <div class="producto-carrito">
+        <img src= ${img} alt="${alt}">
+
+        <div class="datos-producto"> 
+        <p>${nombre}</p>
+        <p>$ ${precio}</p>
+        <p>cantidad = ${cantidad}</p>
+        </div>
+
+        </div> `
+    })).join("");
+}
+
+const btnComprar = document.querySelector(".btn-comprar");
 
 
 const init = () => {
     menuDespegable.addEventListener("click",despeglarMenu); 
     window.addEventListener("resize", onResize);
-    Inicio.addEventListener("click", itemLink);
-    Productos.addEventListener("click", itemLink);
+    /* Inicio.addEventListener("click", removeMenuDes);
+    Productos.addEventListener("click", removeMenuDes); */
     mostrarProductos(ProductosInfo);
-    window.addEventListener("scroll", itemLink);
-    bolsaCompra.addEventListener("click", itemLink);
+    window.addEventListener("scroll", removeMenuDes);
     bolsaCompra.addEventListener("click", carritoCompra);
+    navItem.addEventListener("click", removeMenuDes);
+
+    document.addEventListener("DOMContentLoaded", renderCart);
 }
 
 
