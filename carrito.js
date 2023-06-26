40abfd83b682148f7457bb685dfffe995a8dd8ab
cisshere <cis.shere@gmail.com>
2023-06-25 21:02:32 -0300

    modificando carrito

--- carrito.js
+++ carrito.js
@@ -1,20 +1,14 @@
 var listaCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const contenedorProductosCarrito = document.querySelector(
-  ".productos-contenedor-carrito"
-);
+const contenedorProductosCarrito = document.querySelector(".productos-contenedor-carrito");
 const mensajeCarritoVacio = document.getElementById('mensaje-carrito-vacio');
-const btnTachoResto = document.querySelector(".btn-tacho-resto");
+
 
 const restarCantidad = (producto) => {
   if (producto.cantidad > 1) {
     producto.cantidad -= 1;
-    // btnTachoResto.style.display = "none";
     mostrarCarrito();
-  }
-  /*if (producto == 1){
-    btnTachoResto.style.display = "flex";
-  } */
+  } 
   localStorage.setItem("carrito", JSON.stringify(listaCarrito));
 };
 
@@ -23,6 +17,8 @@ const sumarCantidad = (producto) => {
   mostrarCarrito();
   localStorage.setItem("carrito", JSON.stringify(listaCarrito));
 };
+
+
 
 const mostrarCarrito = () => {
   contenedorProductosCarrito.innerHTML = "";
@@ -59,6 +55,8 @@ const mostrarCarrito = () => {
 
     const btnRestar = div.querySelector(".btn-restar");
     const btnSumar = div.querySelector(".btn-sumar");
+    const btnTachoResto = div.querySelector(".btn-tacho-resto");
+
 
     btnRestar.addEventListener("click", () => {
       restarCantidad(producto);
@@ -67,6 +65,16 @@ const mostrarCarrito = () => {
     btnSumar.addEventListener("click", () => {
       sumarCantidad(producto);
     });
+
+    if (producto.cantidad === 1) {
+      btnRestar.style.display = "none";
+      btnTachoResto.style.display = "flex";
+    } else {
+      btnRestar.style.display = "flex";
+      btnTachoResto.style.display = "none";
+    }
+
+
   });
 
   if (listaCarrito.length === 0) {
@@ -82,13 +90,6 @@ const btnComprar = document.querySelectorAll(".btn-comprar");
 btnComprar.forEach((button, i) => {
   button.addEventListener("click", () => {
     const producto = productosInfo[i];
-    
-    // necesito actualizar la cantidad del producto si es que existe, como busco el producto que necesito?
-    // tengo que recorrer la lista hasta encontrar el producto, como comparo para saber si es el producto que necesito?
-    // para saber si existe comparo el id de cada producto de la lista con el procuto.id que tengo, si no lo encuentro que hago?
-    // si no lo encuentro paso al siguiente item y comparo y si no me quedan mas items no hago nada, y si lo encuentro que hago?
-    // si lo encuentro tengo el indice pero en el scope del foreach, como lo disponibilizo para el resto de la funcion? 
-    // creo una variable para guardar el indice para usarlo despues cuando actualizo el carrito
 
     let indice;
     listaCarrito.forEach((item, index) => {
@@ -97,11 +98,6 @@ btnComprar.forEach((button, i) => {
       }
     });
 
-    // actualizar la cantidad... y como hago eso?
-    // tengo que agarrar el producto de la lista del carrito usando el indice y sumar 1  la cantidad, tengo que actualizar algun dato mas?
-    // tengo que actualizar el local storage para mantener los datos, tengo que hacer algo mas?
-    // tengo que actualizar la lista de la pantalla, como hago eso?
-    // llamo al mostrarCarrito
     if (indice !== undefined) {
       console.log(indice);
       listaCarrito[indice].cantidad += 1;
@@ -109,8 +105,6 @@ btnComprar.forEach((button, i) => {
       producto.cantidad = 1;
       listaCarrito.push(producto);
     }
-
-    
     
     localStorage.setItem("carrito", JSON.stringify(listaCarrito));
     mostrarCarrito();
